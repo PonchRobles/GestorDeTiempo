b3d1e1fb22b7e62283131764c10f451bbee72ef0
Alfonso Rdz <solo-metro@hotmail.com>
2017-09-17 23:17:15 -0500

    Add project files.

---
+++ GestorDeTiempo/InicioDeSesion.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace GestorDeTiempo
+{
+    partial class InicioDeSesion
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnIniciarSession = new System.Windows.Forms.Button();
+            this.txtNombreDeUsuario = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnIniciarSession
+            // 
+            this.btnIniciarSession.Location = new System.Drawing.Point(254, 232);
+            this.btnIniciarSession.Name = "btnIniciarSession";
+            this.btnIniciarSession.Size = new System.Drawing.Size(142, 66);
+            this.btnIniciarSession.TabIndex = 0;
+            this.btnIniciarSession.Text = "Inicar Sesion";
+            this.btnIniciarSession.UseVisualStyleBackColor = true;
+            // 
+            // txtNombreDeUsuario
+            // 
+            this.txtNombreDeUsuario.Location = new System.Drawing.Point(254, 118);
+            this.txtNombreDeUsuario.Name = "txtNombreDeUsuario";
+            this.txtNombreDeUsuario.Size = new System.Drawing.Size(142, 20);
+            this.txtNombreDeUsuario.TabIndex = 1;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(254, 154);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(142, 20);
+            this.txtPassword.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(107, 124);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(96, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Nombre de usuario";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(110, 160);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Contrasena";
+            // 
+            // InicioDeSesion
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(665, 424);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtNombreDeUsuario);
+            this.Controls.Add(this.btnIniciarSession);
+            this.Name = "InicioDeSesion";
+            this.Text = "Iniciar Sesion";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnIniciarSession;
+        private System.Windows.Forms.TextBox txtNombreDeUsuario;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ GestorDeTiempo/Registro.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace GestorDeTiempo
+{
+    partial class Registro
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnRegistrar = new System.Windows.Forms.Button();
+            this.txtNombreDeUsuario = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.lboxTipoDeUsuario = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnRegistrar
+            // 
+            this.btnRegistrar.Location = new System.Drawing.Point(209, 240);
+            this.btnRegistrar.Name = "btnRegistrar";
+            this.btnRegistrar.Size = new System.Drawing.Size(124, 68);
+            this.btnRegistrar.TabIndex = 0;
+            this.btnRegistrar.Text = "Registrar";
+            this.btnRegistrar.UseVisualStyleBackColor = true;
+            // 
+            // txtNombreDeUsuario
+            // 
+            this.txtNombreDeUsuario.Location = new System.Drawing.Point(209, 67);
+            this.txtNombreDeUsuario.Name = "txtNombreDeUsuario";
+            this.txtNombreDeUsuario.Size = new System.Drawing.Size(120, 20);
+            this.txtNombreDeUsuario.TabIndex = 1;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(209, 109);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(120, 20);
+            this.txtPassword.TabIndex = 2;
+            // 
+            // lboxTipoDeUsuario
+            // 
+            this.lboxTipoDeUsuario.FormattingEnabled = true;
+            this.lboxTipoDeUsuario.Location = new System.Drawing.Point(209, 139);
+            this.lboxTipoDeUsuario.Name = "lboxTipoDeUsuario";
+            this.lboxTipoDeUsuario.Size = new System.Drawing.Size(120, 95);
+            this.lboxTipoDeUsuario.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(96, 73);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(96, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Nombre de usuario";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(99, 115);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Contrasena";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(56, 139);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(147, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Selecciona un tipo de usuario";
+            // 
+            // Registro
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(538, 392);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lboxTipoDeUsuario);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtNombreDeUsuario);
+            this.Controls.Add(this.btnRegistrar);
+            this.Name = "Registro";
+            this.Text = "Registro";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnRegistrar;
+        private System.Windows.Forms.TextBox txtNombreDeUsuario;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.ListBox lboxTipoDeUsuario;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
